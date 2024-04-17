b6fcc83e2868eefe6c37b8c58b7a23879b32126a
ElinaNechaieva <45913317+ElinaNechaieva@users.noreply.github.com>
2024-04-17 16:31:47 +0300

    Apply suggestions from code review
    changes
    
    Co-authored-by: Oleksii Budnik <63243136+BudnikOleksii@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,7 @@
 
 export const Person = ({ person}) => {
-  
   const { name, age, sex, isMarried, partnerName } = person;
+  
   return (
   <section className = "Person">
     <h2 className="Person__name">My name is {name}</h2>
